2c5e823f2d994c7eae1333c061ca00f34429e3eb
임희조 <hijo@naver.com>
2013-07-29 14:42:23 +0900

    sherlock

--- WaterHeart/src/nexters/waterheart/MainActivity.java
+++ WaterHeart/src/nexters/waterheart/MainActivity.java
@@ -1,10 +1,10 @@
 package nexters.waterheart;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class MainActivity extends SherlockFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +13,5 @@ public class MainActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
     
 }

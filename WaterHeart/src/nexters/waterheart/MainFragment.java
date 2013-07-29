88247ead3132d3f46b86386fd987b31745e1682c
임희조 <hijo@naver.com>
2013-07-29 15:07:37 +0900

    Fragment

---
+++ WaterHeart/src/nexters/waterheart/MainFragment.java
@@ -0,0 +1,16 @@
+package nexters.waterheart;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class MainFragment extends Fragment{
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}

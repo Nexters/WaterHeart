d652062720824674da4824bec269da79877b3179
LimHeeJho <hijho93@gmail.com>
2013-08-08 11:44:23 +0900

    background

--- WaterHeart/gen/nexters/waterheart/R.java
+++ WaterHeart/gen/nexters/waterheart/R.java
@@ -915,7 +915,8 @@ containing a value of this type.
         public static final int abs__textfield_searchview_right_holo_dark=0x7f02006d;
         public static final int abs__textfield_searchview_right_holo_light=0x7f02006e;
         public static final int abs__toast_frame=0x7f02006f;
-        public static final int ic_launcher=0x7f020070;
+        public static final int back_04_04_04=0x7f020070;
+        public static final int ic_launcher=0x7f020071;
     }
     public static final class id {
         public static final int abs__action_bar=0x7f050020;

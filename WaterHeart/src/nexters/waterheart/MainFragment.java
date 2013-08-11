2f829c5ff9212b02e0c2a81611ab5f8f62479106
treehouse <treehouse@naver.com>
2013-08-11 10:19:07 +0900

    gm

--- WaterHeart/src/nexters/waterheart/MainFragment.java
+++ WaterHeart/src/nexters/waterheart/MainFragment.java
@@ -7,7 +7,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import com.actionbarsherlock.app.SherlockFragment;

23bd78aad369ac78d5005b20b8de472d6dd32e1e
treehouse <treehouse@naver.com>
2013-08-12 15:11:45 +0900

    doing history

--- WaterHeart/src/nexters/waterheart/ClickManager.java
+++ WaterHeart/src/nexters/waterheart/ClickManager.java
@@ -23,10 +23,13 @@ public class ClickManager implements View.OnClickListener{
 		// TODO Auto-generated method stub
 		if(onclick_num==1){
 			historySwap(v.getId());
-			
 		}
+		
 	}
 	
+	/*
+	 * 아오 ㅋㅋㅋ 내가봐도 지저분한데 몰겠다 걍 되기만하면되지
+	 */
 	public void historySwap(int resId){
 		switch(resId){
 		case R.id.history_heart_6:
@@ -37,6 +40,52 @@ public class ClickManager implements View.OnClickListener{
 				mActivity.findViewById(R.id.resultspage_change_6_02).setVisibility(View.GONE);
 				mActivity.findViewById(R.id.resultspage_change_6_01).setVisibility(View.VISIBLE);
 			}
+			break;
+		case R.id.history_heart_5:
+			if(mActivity.findViewById(R.id.resultspage_change_5_01).getVisibility()==View.VISIBLE){
+				mActivity.findViewById(R.id.resultspage_change_5_02).setVisibility(View.VISIBLE);
+				mActivity.findViewById(R.id.resultspage_change_5_01).setVisibility(View.GONE);
+			}else{
+				mActivity.findViewById(R.id.resultspage_change_5_02).setVisibility(View.GONE);
+				mActivity.findViewById(R.id.resultspage_change_5_01).setVisibility(View.VISIBLE);
+			}
+			break;
+		case R.id.history_heart_4:
+			if(mActivity.findViewById(R.id.resultspage_change_4_01).getVisibility()==View.VISIBLE){
+				mActivity.findViewById(R.id.resultspage_change_4_02).setVisibility(View.VISIBLE);
+				mActivity.findViewById(R.id.resultspage_change_4_01).setVisibility(View.GONE);
+			}else{
+				mActivity.findViewById(R.id.resultspage_change_4_02).setVisibility(View.GONE);
+				mActivity.findViewById(R.id.resultspage_change_4_01).setVisibility(View.VISIBLE);
+			}
+			break;
+		case R.id.history_heart_3:
+			if(mActivity.findViewById(R.id.resultspage_change_3_01).getVisibility()==View.VISIBLE){
+				mActivity.findViewById(R.id.resultspage_change_3_02).setVisibility(View.VISIBLE);
+				mActivity.findViewById(R.id.resultspage_change_3_01).setVisibility(View.GONE);
+			}else{
+				mActivity.findViewById(R.id.resultspage_change_3_02).setVisibility(View.GONE);
+				mActivity.findViewById(R.id.resultspage_change_3_01).setVisibility(View.VISIBLE);
+			}
+			break;
+		case R.id.history_heart_2:
+			if(mActivity.findViewById(R.id.resultspage_change_2_01).getVisibility()==View.VISIBLE){
+				mActivity.findViewById(R.id.resultspage_change_2_02).setVisibility(View.VISIBLE);
+				mActivity.findViewById(R.id.resultspage_change_2_01).setVisibility(View.GONE);
+			}else{
+				mActivity.findViewById(R.id.resultspage_change_2_02).setVisibility(View.GONE);
+				mActivity.findViewById(R.id.resultspage_change_2_01).setVisibility(View.VISIBLE);
+			}
+			break;
+		case R.id.history_heart_1:
+			if(mActivity.findViewById(R.id.resultspage_change_1_01).getVisibility()==View.VISIBLE){
+				mActivity.findViewById(R.id.resultspage_change_1_02).setVisibility(View.VISIBLE);
+				mActivity.findViewById(R.id.resultspage_change_1_01).setVisibility(View.GONE);
+			}else{
+				mActivity.findViewById(R.id.resultspage_change_1_02).setVisibility(View.GONE);
+				mActivity.findViewById(R.id.resultspage_change_1_01).setVisibility(View.VISIBLE);
+			}
+			break;
 		}
 	}
 

b4e8fce8cbdf1a8f18d167fcb389654a279961b6
LimHeeJho <hijho93@gmail.com>
2013-08-12 19:46:21 +0900

    test

--- WaterHeart/src/nexters/waterheart/HeartManager.java
+++ WaterHeart/src/nexters/waterheart/HeartManager.java
@@ -10,6 +10,7 @@ import java.util.Locale;
 import nexters.waterheart.db.DBManager;
 import nexters.waterheart.dto.Write;
 import android.app.Activity;
+import android.util.Log;
 
 public class HeartManager {
 
@@ -29,18 +30,20 @@ public class HeartManager {
 		int no;
 		int water = cup;
 		Write write = new Write();
+		String log = null;
 		
 		no = db.getWritesCount();
-		write = db.getWrite(no);
-		water += Integer.parseInt(write.getWater());
-
-		write.setWater(String.valueOf(water));
-		db.addWrite(write);
-
-		no = db.getWritesCount();
-		write = db.getWrite(no);
-		water = Integer.parseInt(write.getWater());
-
+		Log.d("no: ", String.valueOf(no));
+//		write = db.getWrite(no);
+//		water += Integer.parseInt(write.getWater());
+//
+//		write.setWater(String.valueOf(water));
+//		db.addWrite(write);
+//
+//		no = db.getWritesCount();
+//		write = db.getWrite(no);
+//		water = Integer.parseInt(write.getWater());
+//
 		return water; // 현재까지 마신 물의 양 반환이니까 메인에서 양 출력할 때 사용하면 됨
 	}
 

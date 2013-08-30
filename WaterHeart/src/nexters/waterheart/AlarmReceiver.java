4e2d72705506015fa38cc6c56faa3f6ca2c8390e
LimHeeJho <hijho93@gmail.com>
2013-08-30 23:52:23 +0900

    notification

--- WaterHeart/src/nexters/waterheart/AlarmReceiver.java
+++ WaterHeart/src/nexters/waterheart/AlarmReceiver.java
@@ -1,20 +1,26 @@
 package nexters.waterheart;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
 	private static Activity activity;
+	private static String s;
 
 	public AlarmReceiver() {
 		super();
 	}
 
-	public AlarmReceiver(Activity activity) {
+	public AlarmReceiver(Activity activity, String s) {
 		this.activity = activity;
+		this.s = s;
 	}
 
 	@Override
@@ -22,6 +28,25 @@ public class AlarmReceiver extends BroadcastReceiver {
 		// TODO Auto-generated method stub
 		HeartManager heartManager = new HeartManager(activity);
 		heartManager.heartReset();
+		
+		NotificationManager notifier = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		Notification notify = new Notification(R.drawable.actionbar_logo, "목표달성에 실패했어요.",
+				System.currentTimeMillis()); 
+
+		Intent intent2 = new Intent(context, MainActivity.class);
+		PendingIntent pender = PendingIntent
+				.getActivity(context, 0, intent2, 0);
+
+		notify.setLatestEventInfo(context, "Water Heart", s, pender);
+
+		notify.flags |= Notification.FLAG_AUTO_CANCEL;
+		notify.vibrate = new long[] { 200, 200, 500, 300 };
+		// notify.sound=Uri.parse("file:/");
+		notify.number++;
+
+		notifier.notify(1, notify);
 	}
 
 }
--- WaterHeart/src/nexters/waterheart/MainActivity.java
+++ WaterHeart/src/nexters/waterheart/MainActivity.java
@@ -1,11 +1,15 @@
 package nexters.waterheart;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.Random;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -18,6 +22,8 @@ import com.actionbarsherlock.app.SherlockFragmentActivity;
 public class MainActivity extends SherlockFragmentActivity {
 	SectionsPagerAdapter mSectionsPagerAdapter;
 	ViewPager mViewPager;
+	String s;
+	AlarmReceiver mReceiver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +35,30 @@ public class MainActivity extends SherlockFragmentActivity {
 
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
+
 		alarm(MainActivity.this);
 	}
 
 	public void alarm(Context context) {
-
+		Random r = new Random();
+		int num = r.nextInt(3) + 1;
+		//랜덤안됨
+		switch (num) {
+		case 1:
+			s = "내일은 잘 할 수 있죠?";
+		case 2:
+			s = "오늘은 실패했지만 내일은 성공해요~!";
+		case 3:
+			s = "물 마시는 습관 만들기가 쉽지 않죠? 그래도 화이팅!";
+		}
+		mReceiver = new AlarmReceiver(MainActivity.this, s);
 		final AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-		AlarmReceiver mReceiver = new AlarmReceiver(MainActivity.this);
+
 		Intent intent = new Intent("ALARM");
 		final PendingIntent sender = PendingIntent.getBroadcast(
 				MainActivity.this, 0, intent, 0);
-		
-		Calendar calendar = Calendar.getInstance();
+
+		Calendar calendar = new GregorianCalendar(Locale.KOREA);
 		calendar.setTimeInMillis(System.currentTimeMillis());
 		calendar.set(Calendar.HOUR_OF_DAY, 24);
 		calendar.set(Calendar.MINUTE, 0);
@@ -50,6 +68,27 @@ public class MainActivity extends SherlockFragmentActivity {
 		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
 				calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, sender);
 	}
+
+//	@Override
+//	public void onResume() {
+//		super.onResume();
+//		Random r = new Random();
+//		int num = r.nextInt(3) + 1;
+//		switch (num) {
+//		case 1:
+//			s = "내일은 잘 할 수 있죠?";
+//		case 2:
+//			s = "오늘은 실패했지만 내일은 성공해요~!";
+//		case 3:
+//			s = "물 마시는 습관 만들기가 쉽지 않죠? 그래도 화이팅!";
+//		}
+//	}
+
+//	@Override
+//	protected void onDestroy() {
+//		super.onDestroy();
+//		unregisterReceiver(mReceiver);
+//	}
 
 	public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
@@ -87,7 +126,6 @@ public class MainActivity extends SherlockFragmentActivity {
 			else
 				return "History";
 		}
-
 	}
 
 }

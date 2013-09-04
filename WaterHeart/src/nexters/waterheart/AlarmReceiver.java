02fa6e1117befda32df4d5a67d8c66b48d0e54ae
LimHeeJho <hijho93@gmail.com>
2013-09-04 21:06:23 +0900

    alarm except reboot

--- WaterHeart/src/nexters/waterheart/AlarmReceiver.java
+++ WaterHeart/src/nexters/waterheart/AlarmReceiver.java
@@ -1,7 +1,5 @@
 package nexters.waterheart;
 
-import java.util.Random;
-
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -9,46 +7,50 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
 	private static Activity activity;
-	private static String s;
-
+	private String s = null;
+	
 	public AlarmReceiver() {
 		super();
 	}
 
-	public AlarmReceiver(Activity activity, String s) {
+	public AlarmReceiver(Activity activity) {
 		this.activity = activity;
-		this.s = s;
 	}
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
+		if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+			Log.e("TAG", "Boot completed");
+		}
 		HeartManager heartManager = new HeartManager(activity);
 		heartManager.heartReset();
-
-		//if (intent.getAction().equals("FAIL")) {
+		s = intent.getStringExtra("s");
+		if (intent.getBooleanExtra("success", true) == false) {
 			NotificationManager notifier = (NotificationManager) context
 					.getSystemService(Context.NOTIFICATION_SERVICE);
 
 			Notification notify = new Notification(R.drawable.actionbar_logo,
-					"오늘이 5분 남았어요!!", System.currentTimeMillis());
+					"목표 달성 실패~", System.currentTimeMillis());
 
 			Intent intent2 = new Intent(context, MainActivity.class);
 			PendingIntent pender = PendingIntent.getActivity(context, 0,
 					intent2, 0);
 
-			notify.setLatestEventInfo(context, "Water Heart", "오늘이 5분 남았어요!! 오늘의 달성도를 볼까요?", pender);
-
+			notify.setLatestEventInfo(context, "Water Heart",
+					s, pender);
 			notify.flags |= Notification.FLAG_AUTO_CANCEL;
 			notify.vibrate = new long[] { 200, 100, 500, 300 };
 			// notify.sound=Uri.parse("file:/");
 			notify.number++;
 
 			notifier.notify(1, notify);
-		//}
+		}
+
 	}
 
 }
--- WaterHeart/src/nexters/waterheart/MainActivity.java
+++ WaterHeart/src/nexters/waterheart/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -28,11 +29,15 @@ public class MainActivity extends SherlockFragmentActivity {
 	private static final String FRAGMENT_TAG_CUSTOM = "CUSTOM";
 	private static final String FRAGMENT_TAG_CUSTOM02 = "CUSTOM02";
 
+	public MainActivity() {
+		super();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// if (Intent.ACTION_BOOT_COMPLETED != null)
 		setContentView(R.layout.activity_main);
-		// heartManager.init();
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		mSectionsPagerAdapter = new SectionsPagerAdapter(
 				getSupportFragmentManager());
@@ -44,32 +49,40 @@ public class MainActivity extends SherlockFragmentActivity {
 	public void alarm(Context context) {
 		Random r = new Random();
 		int num = r.nextInt(3) + 1;
-		// 스타틱이라 랜덤안됨
 		switch (num) {
 		case 1:
 			s = "내일은 잘 할 수 있죠?";
+			break;
 		case 2:
 			s = "오늘은 실패했지만 내일은 성공해요~!";
+			break;
 		case 3:
 			s = "물 마시는 습관 만들기가 쉽지 않죠? 그래도 화이팅!";
+			break;
 		}
-		mReceiver = new AlarmReceiver(MainActivity.this, s);
-//		IntentFilter filler = new IntentFilter();
-//		filler.addAction("ALARM");
-//		filler.addAction("FAIL");
-//		this.registerReceiver(mReceiver, filler);
-		
+
+		mReceiver = new AlarmReceiver(MainActivity.this);
+		// IntentFilter filler = new IntentFilter();
+		// filler.addAction("ALARM");
+		// filler.addAction("android.intent.action.BOOT_COMPLETED");
+		// this.registerReceiver(mReceiver, filler);
+
 		final AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 		Intent intent = new Intent("ALARM");
-//		if (heartManager.mainHeartShow() < MainFragment.totalWater)
-//			intent.setAction("FAIL");
-		final PendingIntent sender = PendingIntent.getBroadcast(
-				MainActivity.this, 0, intent, 0);
+		if (heartManager.mainHeartShow() < MainFragment.totalWater)
+			intent.putExtra("success", false);
+		else
+			intent.putExtra("success", true);
+		intent.putExtra("s", s);
+
+		final PendingIntent sender = PendingIntent
+				.getBroadcast(MainActivity.this, 0, intent,
+						PendingIntent.FLAG_UPDATE_CURRENT);
 
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(System.currentTimeMillis());
-		calendar.set(Calendar.HOUR_OF_DAY, 23);
-		calendar.set(Calendar.MINUTE, 55);
+		calendar.set(Calendar.HOUR_OF_DAY, 24);
+		calendar.set(Calendar.MINUTE, 0);
 		calendar.set(Calendar.SECOND, 0);
 		calendar.set(Calendar.MILLISECOND, 0);
 
@@ -81,9 +94,7 @@ public class MainActivity extends SherlockFragmentActivity {
 	public void onResume() {
 		// TODO Auto-generated method stub
 		init();
-		// alarmFiller(this);
-		// ha();
-		alarm(this);
+		// alarm(this);
 		super.onResume();
 	}
 
@@ -98,32 +109,39 @@ public class MainActivity extends SherlockFragmentActivity {
 		CupCustomizingFragment fragment01 = null;
 		CustomFragment01 fragment02 = null;
 		CustomFragment02 fragment03 = null;
-		fragment01 = (CupCustomizingFragment)getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG_CUPCUSTOM);
-		fragment02 = (CustomFragment01)getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG_CUSTOM);
-		fragment03 = (CustomFragment02)getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG_CUSTOM02);
-		if((fragment01 == null) && (fragment02 == null) && (fragment03 == null)){
+		fragment01 = (CupCustomizingFragment) getSupportFragmentManager()
+				.findFragmentByTag(FRAGMENT_TAG_CUPCUSTOM);
+		fragment02 = (CustomFragment01) getSupportFragmentManager()
+				.findFragmentByTag(FRAGMENT_TAG_CUSTOM);
+		fragment03 = (CustomFragment02) getSupportFragmentManager()
+				.findFragmentByTag(FRAGMENT_TAG_CUSTOM02);
+		if ((fragment01 == null) && (fragment02 == null)
+				&& (fragment03 == null)) {
 			new AlertDialog.Builder(this)
-			.setTitle("정말 종료하시겠습니까?")
-			.setMessage("종료하실꺼유? ㅠㅠ")
-			.setNegativeButton("아니오", null)
-			.setPositiveButton("네", new DialogInterface.OnClickListener(){
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO Auto-generated method stub
-					finish();
-				}
-			}).show();
-		}else{
+					.setTitle("정말 종료하시겠습니까?")
+					.setMessage("종료하실꺼유? ㅠㅠ")
+					.setNegativeButton("아니오", null)
+					.setPositiveButton("네",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// TODO Auto-generated method stub
+									finish();
+								}
+							}).show();
+		} else {
 			super.onBackPressed();
-		
+
 		}
 	}
-	
+
 	@Override
 	public void onPause() {
 		super.onPause();
-		//this.unregisterReceiver(mReceiver);
+		alarm(this);
+		// this.unregisterReceiver(mReceiver);
 	}
 
 	public class SectionsPagerAdapter extends FragmentPagerAdapter {
